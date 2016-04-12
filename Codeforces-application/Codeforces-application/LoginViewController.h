c3c5125473b0ca5778876c12c4974ba24fb5f4da
sergmiller <sergey.miller@phystech.edu>
2016-04-12 20:25:17 +0300

    added class LoginViewController

---
+++ Codeforces-application/Codeforces-application/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  CF-app
+//
+//  Created by Сергей Миллер on 12.04.16.
+//  Copyright © 2016 ioscourse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
